f496a8e4b0ef9bf1e5f54e18bb18520e3f42b94d
Danish <danishchopra11@gmail.com>
2023-09-14 00:28:06 +0530

    first commit

---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Todoitem } from './Todoitem';
+
+export const Todos = (props) => {
+    let myStyle={
+        minHeight:"70vh"
+    }
+  return (
+    <div className='container my-3' style={myStyle}>
+        <h3 className='my-3'>
+            Todos List
+        </h3>
+        {props.todos.length==0? "No Todos to display": props.todos.map((todo)=>
+        {
+return <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+        }
+        )
+    }
+       
+        
+        </div>
+  )
+}
